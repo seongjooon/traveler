9ec6bf5790a11bfcedc140d013eaf3a002debf9c
MrKamake <seongjooon@gmail.com>
2019-12-18 11:40:01 +0900

    Create List component & scss file

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Header from './Header/Header';
+import List from './List/List';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <List />
       </div>
     );
   }
---
+++ src/components/List/List.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './List.scss';
+
+class List extends Component {
+  render() {
+    return <div className="List"></div>;
+  }
+}
+
+export default List;
