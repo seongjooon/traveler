d804dcc4472bd48396e1feb7d913c1707d9e45e7
MrKamake <seongjooon@gmail.com>
2019-12-18 11:39:34 +0900

    Create Header component & scss file

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Header from './Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Header.scss';
+
+class Header extends Component {
+  render() {
+    return <div className="Header"></div>;
+  }
+}
+
+export default Header;
